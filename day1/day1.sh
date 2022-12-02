81e9fdcf0d48f044362558c524ece21ce2bfe488
David Curran <david.curran@smarsh.com>
2022-12-02 09:10:43 +0000

    add day1

---
+++ day1/day1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+elves=()
+
+cal=0
+while IFS= read -r l; do
+  if [[ $l != "" ]]; then
+    cal=$((cal + l))
+  else
+    elves+=("$cal")
+    cal=0
+  fi
+done < input.txt
+
+#echo "${elves[*]}" | sort -n 
+top=$(IFS=$'\n' sort <<<"${elves[*]}" | sort -n -r | head -1)
+
+echo "Top elf has $top calories in their bag"
+
+three=$(IFS=$'\n' sort <<<"${elves[*]}" | sort -n -r | head -3)
+
+topthree=0
+for n in $three; do
+   topthree=$(( topthree + n ))
+done
+
+echo $topthree
